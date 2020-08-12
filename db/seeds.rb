ecb1b7f5df1376648439d31a6decd81512ed2cf6
Cloe Furlan <cloe.furlan@yahoo.fr>
2020-08-12 16:58:21 -0400

    Init the MVC

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Great appartment near Montmartre',
+  address: 'rue des Abbesses, Paris',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Spacious 2 bedroom in the city center',
+  address: 'Downing street',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 200,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Studio close to The Louvre',
+  address: 'avenue Royale, Paris',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 120,
+  number_of_guests: 2
+)
